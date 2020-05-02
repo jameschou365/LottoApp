7f4f47c8ab11290cb15e2752d04bb7918369b4b4
jameschou365 <58102173+jameschou365@users.noreply.github.com>
2020-05-02 00:02:50 -0700

    Update BC49ajax.js

--- js/BC49ajax.js
+++ js/BC49ajax.js
@@ -5,7 +5,7 @@ function makeplot() {
 
 function processData(allRows) {
     console.log(allRows);
-    let x = [], y6 = [], y5 = [], y4 = [], y3 = [], y2 = [], y1 = [];
+    let x = [], y6 = [], y5 = [], y4 = [], y3 = [];
 
     let row;
     for (let i = 0; i < allRows.length; i++) {
@@ -14,18 +14,16 @@ function processData(allRows) {
         y6.push(row['S6']);
         y5.push(row['S5']);
         y4.push(row['S4']);
-        y3.push(row['S3']);
-        y2.push(row['S2']);
-        y1.push(row['S1']);
+        y3.push(row['S3'])
     }
-    console.log( 'X',x, 'Y6',y6, 'Y5',y5, 'Y4',y4, 'Y3',y3, 'Y2',y2, 'Y1',y1 );
-    makePlotly( x, y6, y5, y4, y3, y2, y1);
+    console.log( 'X',x, 'Y6',y6, 'Y5',y5, 'Y4',y4, 'Y3',y3);
+    makePlotly( x, y6, y5, y4, y3);
 }
 
-function makePlotly(x, y6, y5, y4, y3, y2, y1) {
+function makePlotly(x, y6, y5, y4, y3) {
     var plotDiv = document.getElementById("plot");
     var traces = [{ x: x, y: y6 }, { x: x, y: y5 }, { x: x, y: y4 },
-        { x: x, y: y3 }, { x: x, y: y2 }, { x: x, y: y1 }];
+        { x: x, y: y3 }];
 
     var layout = {
         barmode: 'relative',
